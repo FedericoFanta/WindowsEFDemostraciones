5155e5ad8021857fa1c2511b2642fcd22767bf25
Santiago Fantacone <Santiago Fantacone@DESKTOP-LGQVMKN>
2022-11-07 17:09:41 -0300

    Agregar archivos de proyecto.

---
+++ WindowsEF/Data/DBProductionContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using WindowsEF.Models;
+namespace WindowsEF.Data
+
+{
+    public class DBProductionContext:DbContext
+    {
+        public DBProductionContext() : base("keyDBProduction") { }
+
+        //por cada modelo una propiedad DBSET
+
+        public DbSet<Categoria> Categorias { get; set; }
+
+        public DbSet<Producto> Productos { get; set; }
+
+
+    }
+}
---
+++ WindowsEF/Models/Categoria.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsEF.Models
+{
+    [Table("Categoria")]
+    public class Categoria
+    {
+        public int Id { get; set; }
+
+        [Required] // campo obligatorio- en DB not null
+        [Column(TypeName ="varchar")] // tipo de dato de slqserver
+        [StringLength(50)]//longitud maxima de la cadena
+        public string Nombre { get; set; }
+    }
+}
